990e480046e52360af66536b7ede3a7a14c933e3
Albano <77249799+Yokekomo@users.noreply.github.com>
2022-01-17 17:14:54 +0100

    Actualización

--- Albano_Chicharro_Altur_Ejercicio_05_Intensivo_Python.py
+++ Albano_Chicharro_Altur_Ejercicio_05_Intensivo_Python.py
@@ -11,9 +11,27 @@ Aprender a trabajar con ficheros
 Usar la librería pandas de Python
 
 """
+import csv
 
 import pandas as pd
 
-archivo_excel = pd.read_excel(r'\Descargas\archivo_excel.xlsx')
-archivo_excel.to_csv(r'\Descargas\nuevo_archivo.csv', index=None, header=True)
+archivo_excel = pd.read_excel(r'\Descargas\excel.xlsx')
+nombre = []
+email = []
+telf = []
+diccionario = []
 
+for i in archivo_excel['Nombre'[:]]:
+    nombre.append(i)
+for j in archivo_excel['Email'[:]]:
+    email.append(j)
+for k in archivo_excel['Telf'[:]]:
+    telf.append(k)
+
+for l in range(len(nombre)):
+    dict1 = {'Nombre': nombre[l], 'Email': email[l], 'Telf': telf[l]}
+    diccionario.append(dict1)
+
+
+df = pd.DataFrame(diccionario)
+df.to_csv(r'\Descargas\nuevo.csv', index=False)
