288beefa636308f480ec6654c93d4478b1b8c842
Albano <77249799+Yokekomo@users.noreply.github.com>
2022-01-20 15:15:04 +0100

    Ejercicio corregido con creador de hash md5

---
+++ Albano_Chicharro_Altur_Ejercicio_06b_Intensivo_Python.py
@@ -0,0 +1,73 @@
+# Albano Chicharro Altur
+# Ejercicio 6b
+# 19/01/2022
+
+# Hecho con hash
+
+"""   Ejercicio 6
+
+Enunciado:
+
+Crea una función que convierta un password (entre 6 y 12 caracteres) es una cadena de texto alfanumérica de
+32 caracteres. La función SIEMPRE debe devolver el mismo resultado para la misma entrada.
+
+Objetivo:
+
+- Aprender a manejar los bucles y las cadena de texto.
+- Mejorar la capacidad algorítmica.
+
+"""
+import hashlib
+
+
+class Encriptar:
+
+    def __init__(self):
+        self.__password = None
+        self.__crypto = None
+
+    def pedirPassword(self):
+
+        while True:
+            try:
+                self.__password = input('Introduce una Contraseña:\n')
+                if (len(self.__password) >= 6) and (len(self.__password) <= 12):
+                    if self.__password.isalnum():
+                        return self.diccionario()
+                    else:
+                        print('La contraseña debe de ser alfanumérica.')
+                else:
+                    print('La contraseña debe contener entre 6 y 12 caracteres.')
+            except:
+                print('Ha ocurrido un ERROR')
+
+    def encriptar_password(self):
+
+        textoMD5 = hashlib.md5()
+        textoMD5.update(self.__password.encode("UTF-8"))
+        self.__crypto = str(textoMD5.hexdigest())
+        return self.__crypto
+
+    def diccionario(self):
+
+        diccionario = {'7uU4cA2hC1kD8hT0cX3qC4bA6hA9dS0dQ': 'albano12',
+                       '9vG8hY7kG9gD4bU3mN0cA5cN9fX5nN6sM': 'albano13',
+                       '8wI7bA7xS8zW9zR4iK9gX4eS6bK4qN2hH': 'albano14'}
+
+        try:
+            if self.__password in diccionario.values():
+                print('La contraseña ya existe')
+                self.pedirPassword()
+            else:
+                self.encriptar_password()
+                diccionario.update({self.__crypto: self.__password})
+                print('La contraseña no existe - Contraseña almacenada', self.__crypto)
+        except:
+            print('Algo salio mal en el diccionario')
+
+
+encriptar_un_password = Encriptar()
+encriptar_un_password.pedirPassword()
+
+
+
